1cdacda1b3ddcf20edff8a6654860297ee8dd7a0
Deon Marais <deon.marais@gmail.com>
2016-11-17 11:00:58 +0100

    Menu Items added with KnockoutBindings

--- js/model.js
+++ js/model.js
@@ -1,48 +1,51 @@
-var model = {
-  markers: [ {
-    title: 'Castle Blankenberg',
-      lat: 50.764547,
-      lng: 7.3593,
-      url: 'http://www.butterflyhouse.org',
-      highlight: ko.observable(false),
-
-
+//An Array of places of interesting places
+var places = [
+ {
+  title: 'Castle Blankenberg',
+  position: {
+    lat: 50.764547,
+    lng: 7.3593
   },
-  {
-    title: 'Uckerath',
-      lat: 50.734131,
-      lng: 7.3676475,
-      url: 'http://www.uckerath.de',
-      highlight: ko.observable(false),
+  url: 'http://www.butterflyhouse.org',
 
 
+
+}, {
+  title: 'Uckerath',
+  position: {
+    lat: 50.734131,
+    lng: 7.3676475
   },
-  {
-    title: 'Kur Theater',
-      lat: 50.7751605,
-      lng: 7.2744653,
-      url: 'http://www.kurtheaterhennef.de',
-      highlight: ko.observable(false),
+  url: 'http://www.uckerath.de',
 
 
+
+}, {
+  title: 'Kur Theater',
+  postion: {
+    lat: 50.7751605,
+    lng: 7.2744653
   },
-  {
-    title: 'The River Sieg Estuary',
-      lat: 50.768611,
-      lng: 7.075556,
-      url: 'https://en.wikipedia.org/wiki/Sieg',
-      highlight: ko.observable(false),
+  url: 'http://www.kurtheaterhennef.de',
 
 
+
+}, {
+  title: 'The River Sieg Estuary',
+  position: {
+    lat: 50.768611,
+    lng: 7.075556
   },
-  {
-    title: 'Terra Rossa',
-      lat: 50.7737624,
-      lng: 7.3541786,
-      url: 'osteria-terrarossa.de',
-      highlight: ko.observable(false),
+  url: 'https://en.wikipedia.org/wiki/Sieg',
 
 
-  }
-]
-};
+
+}, {
+  title: 'Terra Rossa',
+  position: {
+    lat: 50.7737624,
+    lng: 7.3541786
+  },
+  url: 'osteria-terrarossa.de',
+
+}];
--- js/view.js
+++ js/view.js
@@ -1,5 +1,14 @@
 
-var ViewModel = function(){
+// Place Class from the model a
+var Place = function(obj){
+  var self = this;
+
+  self.title = ko.observable(obj.title);
+  self.postion = ko.observable(obj.position);
+  self.url = ko.observable(obj.url);
+
+
+};
 
 var map;
 var mapOptions =  {
@@ -8,32 +17,63 @@ var mapOptions =  {
 
 
 
-//Initialize the Map
-var mapInit = function(){
+//Initialize the Map given a set of "mapOptions"
+var mapInit = function() {
 
-  map = new google.maps.Map(document.getElementById("map"),mapOptions);
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-
-//Create Marker object using using the array of location objects in the "Model"
-model.markers.forEach(function(obj){
+};
 
 
-var marker = new google.maps.Marker({
 
-  position: {lat:obj.lat, lng:obj.lng},
-  title: obj.title,
-  map:map,
-  animation: google.maps.Animation.DROP,
-  
+var ViewModel = function(){
 
-}
-);
-});
+var self = this;
 
-}();
+  mapInit();
 
+self.placeList = ko.observableArray([]);
+
+  //From the Model bring the Places into the ViewModel
+  places.forEach(function(obj){
+    self.placeList.push(new Place(obj));
+
+
+
+  });
+self.currentPlace = self.placeList()[0];
+console.log(self.currentPlace.title());
 
 
 };
+
+
+  //Create Marker object using using the array of location objects in the "Model"
+
+
+  /*model.markers.forEach(function(obj) {
+
+
+    var marker = new google.maps.Marker({
+
+      position: {
+        lat: obj.lat,
+        lng: obj.lng
+      },
+      title: obj.title,
+      map: map,
+      animation: google.maps.Animation.DROP,
+
+
+    });
+  });*/
+
+
+
+var menuInit = function() {
+}
+
+
+
 
 ko.applyBindings(new ViewModel());
