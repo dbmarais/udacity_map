9015472e4eeaace81b2802caff9d8b421aaa5141
Deon Marais <deon.marais@gmail.com>
2016-11-17 11:55:16 +0100

    info test added to model

--- js/model.js
+++ js/model.js
@@ -3,6 +3,7 @@ var places =  [ {
       position:{lat: 50.764547,
       lng: 7.3593},
       url: 'http://www.butterflyhouse.org',
+      info:"test info"
       highlight: ko.observable(false),
 
 
@@ -12,6 +13,7 @@ var places =  [ {
       position:{lat: 50.734131,
       lng: 7.3676475},
       url: 'http://www.uckerath.de',
+        info:"test info",
       highlight: ko.observable(false),
 
 
@@ -21,6 +23,7 @@ var places =  [ {
       position:{lat: 50.7751605,
       lng: 7.2744653},
       url: 'http://www.kurtheaterhennef.de',
+        info:"test info",
       highlight: ko.observable(false),
 
 
@@ -30,6 +33,7 @@ var places =  [ {
       position:{lat: 50.768611,
       lng: 7.075556},
       url: 'https://en.wikipedia.org/wiki/Sieg',
+        info:"test info",
       highlight: ko.observable(false),
 
 
@@ -39,6 +43,7 @@ var places =  [ {
       position:{lat: 50.7737624,
       lng: 7.3541786},
       url: 'osteria-terrarossa.de',
+        info:"test info",
       highlight: ko.observable(false),
 
 
