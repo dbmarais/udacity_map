e85e74b758dc158152aefdf3e97cc48bc99b4eb2
Deon Marais <deon.marais@gmail.com>
2016-11-17 11:25:18 +0100

    simple markers set

--- js/model.js
+++ js/model.js
@@ -1,50 +1,46 @@
-//An Array of places of interesting places
-var places = [
-  {
+var places =  [ {
     title: 'Castle Blankenberg',
-    position: {
-      lat: 50.764547,
-      lng: 7.3593
-    },
-  },
+      position:{lat: 50.764547,
+      lng: 7.3593},
+      url: 'http://www.butterflyhouse.org',
+      highlight: ko.observable(false),
 
+
+  },
   {
     title: 'Uckerath',
-    position: {
-      lat: 50.734131,
-      lng: 7.3676475
-    },
-    url: 'http://www.uckerath.de',
+      position:{lat: 50.734131,
+      lng: 7.3676475},
+      url: 'http://www.uckerath.de',
+      highlight: ko.observable(false),
 
 
-
-  }, {
+  },
+  {
     title: 'Kur Theater',
-    postion: {
-      lat: 50.7751605,
-      lng: 7.2744653
-    },
-    url: 'http://www.kurtheaterhennef.de',
+      position:{lat: 50.7751605,
+      lng: 7.2744653},
+      url: 'http://www.kurtheaterhennef.de',
+      highlight: ko.observable(false),
 
 
-
-  }, {
+  },
+  {
     title: 'The River Sieg Estuary',
-    position: {
-      lat: 50.768611,
-      lng: 7.075556
-    },
-    url: 'https://en.wikipedia.org/wiki/Sieg',
+      position:{lat: 50.768611,
+      lng: 7.075556},
+      url: 'https://en.wikipedia.org/wiki/Sieg',
+      highlight: ko.observable(false),
 
 
-
-  }, {
+  },
+  {
     title: 'Terra Rossa',
-    position: {
-      lat: 50.7737624,
-      lng: 7.3541786
-    },
-    url: 'osteria-terrarossa.de',
+      position:{lat: 50.7737624,
+      lng: 7.3541786},
+      url: 'osteria-terrarossa.de',
+      highlight: ko.observable(false),
+
 
   }
 ];
